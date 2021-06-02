247bd967cdf8cb0cb14dba0af966b3f71d6d1567
menezesmario <mario082@gmail.com>
2021-06-02 16:59:36 -0300

    Print Data Products / ToString

---
+++ StockProducts/StockProducts/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace StockProducts
+{
+    class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public double StockValue()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantity
+                + " unidades, Total: $ "
+                + StockValue().ToString("F2", CultureInfo.InvariantCulture)
+                ;
+        }
+    }
+}
--- StockProducts/StockProducts/Program.cs
+++ StockProducts/StockProducts/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace StockProducts
 {
@@ -10,6 +7,20 @@ namespace StockProducts
     {
         static void Main(string[] args)
         {
+            Product p = new Product();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Name = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
         }
     }
 }
+
+
+
